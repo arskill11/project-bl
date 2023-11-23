8f280e80a31e132dfd8465f786071dbcd7ae13d2
arskill11 <atrinozenko3@gmail.com>
2023-11-23 23:07:24 +0300

    done

---
+++ alrina.js
@@ -0,0 +1,44 @@
+const $body = $("body");
+const $header = $(".page-header");
+const $navCollapse = $(".navbar-collapse");
+const scrollClass = "scroll";
+$(window).on("scroll", () => {
+  if (this.matchMedia("(min-width: 992px)").matches) {
+    const scrollY = $(this).scrollTop();
+    scrollY > 0
+      ? $body.addClass(scrollClass)
+      : $body.removeClass(scrollClass);
+  } else {
+    $body.removeClass(scrollClass);
+  }
+});
+$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
+  const href = $(this).attr("href");
+  $("html, body").animate({
+    scrollTop: $(href).offset().top - 71
+  }, 600);
+});
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
